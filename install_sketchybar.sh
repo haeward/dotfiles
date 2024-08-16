2520772771ccf7427149e376710cfc798f742f50
Haeward <code@haeward.com>
2024-08-16 11:13:53 +0800

    ✨ feat: add sketchybar install script

---
+++ install_sketchybar.sh
@@ -0,0 +1,20 @@
+echo "Installing sketchybar..."
+
+# Packages
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
+echo "Cloning configurations..."
+git clone https://github.com/haeward/dotfiles.git /tmp/dotfiles
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+mv /tmp/dotfiles/sketchybar $HOME/.config/sketchybar
+rm -rf /tmp/dotfiles
+brew services start sketchybar
