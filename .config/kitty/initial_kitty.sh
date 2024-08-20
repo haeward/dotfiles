616f4346c04db77254ccdfc61a5f1d0e661948ab
Haeward <code@haeward.com>
2024-08-21 00:09:58 +0800

    🎈 perf: adjust kitty background

--- .config/kitty/initial_kitty.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-screen_width=$(system_profiler SPDisplaysDataType | grep Resolution | awk '{print $2}')
-screen_height=$(system_profiler SPDisplaysDataType | grep Resolution | awk '{print $4}')
-kitty_width=$((screen_width / 2))
-kitty_height=$((screen_height / 2))
-
-kitty --override initial_window_width=${kitty_width} --override initial_window_height=${kitty_height} 2>/dev/null
